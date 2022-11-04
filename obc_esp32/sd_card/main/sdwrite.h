d368a8bde47614d87bf3b347bbc06fded1c569a6
Priyanta Islam <priyantaislam@gmail.com>
2022-11-04 16:58:15 +1100

    sd write component with esp32 build files

---
+++ obc_esp32/sd_card/main/sdwrite.h
@@ -0,0 +1,21 @@
+/* SD card and FAT filesystem example.
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdspi_host.h"
+#include "driver/spi_common.h"
+#include "sdmmc_cmd.h"
+#include "sdkconfig.h"
+
+int sd_write(int MISO, int MOSI, int CLK, int CS, char* data); 
